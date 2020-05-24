4523ab64ee687c0ba32cc6183689132c6bf2e97f
Keviindran Ramachandran <keviinx@yahoo.com>
2020-05-24 02:13:01 +0200

    updated application and database
    Added implementation for add-result for both solo and team. Updated database with all result.

--- application.py
+++ application.py
@@ -23,22 +23,168 @@ def add_result():
 
     return render_template("add-result.html", players=players) 
 
-@app.route("/t1-win")
+@app.route("/t1-win", methods=["GET", "POST"])
 def t1_win():
 
-    flash("T1 added!")
+    if request.method == "POST":
+        # game type was solo
+        if request.form.get("match_type") == 'solo':
+            # get both player's id
+            t1p1 = request.form.get("t1p1")
+            t2p1 = request.form.get("t2p1")
+            #update data in database
+            db.execute("UPDATE players SET w = w + 1 WHERE id = (:id)", id=t1p1)
+            db.execute("UPDATE players SET l = l + 1 WHERE id = (:id)", id=t2p1)
+            #flash message
+            flash("Result added!")
+        #game type was team    
+        elif request.form.get("match_type") == 'team':
+            # get all player's id
+            t1p1 = request.form.get("t1p1")
+            t1p2 = request.form.get("t1p2")
+            t2p1 = request.form.get("t2p1")
+            t2p2 = request.form.get("t2p2")
+            #rearrange the id of the player alphabetically
+            t1order = db.execute("SELECT id FROM players WHERE id = (:t1p1id) OR id = (:t1p2id) ORDER BY name ASC", t1p1id=t1p1, t1p2id=t1p2)
+            t2order = db.execute("SELECT id FROM players WHERE id = (:t2p1id) OR id = (:t2p2id) ORDER BY name ASC", t2p1id=t2p1, t2p2id=t2p2)
+            #check if any team already created
+            t1teamcheck = db.execute("SELECT * FROM teams WHERE p1_id = (:t1p1id) AND p2_id = (:t1p2id)", t1p1id=t1order[0]['id'], t1p2id=t1order[1]['id'])
+            t2teamcheck = db.execute("SELECT * FROM teams WHERE p1_id = (:t2p1id) AND p2_id = (:t2p2id)", t2p1id=t2order[0]['id'], t2p2id=t2order[1]['id'])
+            #team 1 check
+            #if not found
+            if len(t1teamcheck) == 0:
+                db.execute("INSERT INTO teams (p1_id, p2_id, w) VALUES ((:p1_id),(:p2_id),(:w))", p1_id=t1order[0]['id'], p2_id=t1order[1]['id'], w=1)
+                db.execute("UPDATE players SET w = w + 1 WHERE id = (:id)", id=t1order[0]['id'])
+                db.execute("UPDATE players SET w = w + 1 WHERE id = (:id)", id=t1order[1]['id'])
+            #if found
+            else:
+                db.execute("UPDATE teams SET w = w + 1 WHERE p1_id = (:p1_id) AND p2_id = (:p2_id)", p1_id=t1order[0]['id'], p2_id=t1order[1]['id'])
+                db.execute("UPDATE players SET w = w + 1 WHERE id = (:id)", id=t1order[0]['id'])
+                db.execute("UPDATE players SET w = w + 1 WHERE id = (:id)", id=t1order[1]['id'])
+            #team 2 check
+            # if not found
+            if len(t2teamcheck) == 0:
+                db.execute("INSERT INTO teams (p1_id, p2_id, l) VALUES ((:p1_id),(:p2_id),(:l))", p1_id=t2order[0]['id'], p2_id=t2order[1]['id'], l=1)
+                db.execute("UPDATE players SET l = l + 1 WHERE id = (:id)", id=t2order[0]['id'])
+                db.execute("UPDATE players SET l = l + 1 WHERE id = (:id)", id=t2order[1]['id'])
+            #if found
+            else:
+                db.execute("UPDATE teams SET l = l + 1 WHERE p1_id = (:p1_id) AND p2_id = (:p2_id)", p1_id=t2order[0]['id'], p2_id=t2order[1]['id'])
+                db.execute("UPDATE players SET l = l + 1 WHERE id = (:id)", id=t2order[0]['id'])
+                db.execute("UPDATE players SET l = l + 1 WHERE id = (:id)", id=t2order[1]['id'])
+              
+            flash("Result added!")
+    
     return redirect("/add-result")
 
-@app.route("/draw")
+@app.route("/draw", methods=["GET", "POST"])
 def draw():
 
-    flash("Draw added!")
+    if request.method == "POST":
+        # game type was solo
+        if request.form.get("match_type") == 'solo':
+            # get both player's id
+            t1p1 = request.form.get("t1p1")
+            t2p1 = request.form.get("t2p1")
+            #update data in database
+            db.execute("UPDATE players SET d = d + 1 WHERE id = (:id)", id=t1p1)
+            db.execute("UPDATE players SET d = d + 1 WHERE id = (:id)", id=t2p1)
+            #flash message
+            flash("Result added!")
+            
+        #game type was team    
+        elif request.form.get("match_type") == 'team':
+            # get all player's id
+            t1p1 = request.form.get("t1p1")
+            t1p2 = request.form.get("t1p2")
+            t2p1 = request.form.get("t2p1")
+            t2p2 = request.form.get("t2p2")
+            #rearrange the id of the player alphabetically
+            t1order = db.execute("SELECT id FROM players WHERE id = (:t1p1id) OR id = (:t1p2id) ORDER BY name ASC", t1p1id=t1p1, t1p2id=t1p2)
+            t2order = db.execute("SELECT id FROM players WHERE id = (:t2p1id) OR id = (:t2p2id) ORDER BY name ASC", t2p1id=t2p1, t2p2id=t2p2)
+            #check if any team already created
+            t1teamcheck = db.execute("SELECT * FROM teams WHERE p1_id = (:t1p1id) AND p2_id = (:t1p2id)", t1p1id=t1order[0]['id'], t1p2id=t1order[1]['id'])
+            t2teamcheck = db.execute("SELECT * FROM teams WHERE p1_id = (:t2p1id) AND p2_id = (:t2p2id)", t2p1id=t2order[0]['id'], t2p2id=t2order[1]['id'])
+            #team 1 check
+            #if not found
+            if len(t1teamcheck) == 0:
+                db.execute("INSERT INTO teams (p1_id, p2_id, d) VALUES ((:p1_id),(:p2_id),(:d))", p1_id=t1order[0]['id'], p2_id=t1order[1]['id'], d=1)
+                db.execute("UPDATE players SET d = d + 1 WHERE id = (:id)", id=t1order[0]['id'])
+                db.execute("UPDATE players SET d = d + 1 WHERE id = (:id)", id=t1order[1]['id'])
+            #if found
+            else:
+                db.execute("UPDATE teams SET d = d + 1 WHERE p1_id = (:p1_id) AND p2_id = (:p2_id)", p1_id=t1order[0]['id'], p2_id=t1order[1]['id'])
+                db.execute("UPDATE players SET d = d + 1 WHERE id = (:id)", id=t1order[0]['id'])
+                db.execute("UPDATE players SET d = d + 1 WHERE id = (:id)", id=t1order[1]['id'])
+            #team 2 check
+            # if not found
+            if len(t2teamcheck) == 0:
+                db.execute("INSERT INTO teams (p1_id, p2_id, d) VALUES ((:p1_id),(:p2_id),(:d))", p1_id=t2order[0]['id'], p2_id=t2order[1]['id'], d=1)
+                db.execute("UPDATE players SET d = d + 1 WHERE id = (:id)", id=t2order[0]['id'])
+                db.execute("UPDATE players SET d = d + 1 WHERE id = (:id)", id=t2order[1]['id'])
+            #if found
+            else:
+                db.execute("UPDATE teams SET d = d + 1 WHERE p1_id = (:p1_id) AND p2_id = (:p2_id)", p1_id=t2order[0]['id'], p2_id=t2order[1]['id'])
+                db.execute("UPDATE players SET d = d + 1 WHERE id = (:id)", id=t2order[0]['id'])
+                db.execute("UPDATE players SET d = d + 1 WHERE id = (:id)", id=t2order[1]['id'])
+              
+            flash("Result added!")
+
     return redirect("/add-result")
 
-@app.route("/t2-win")
+@app.route("/t2-win", methods=["GET", "POST"])
 def t2_win():
 
-    flash("T2 added!")
+    if request.method == "POST":
+        # game type was solo
+        if request.form.get("match_type") == 'solo':
+            # get both player's id
+            t1p1 = request.form.get("t1p1")
+            t2p1 = request.form.get("t2p1")
+            #update data in database
+            db.execute("UPDATE players SET l = l + 1 WHERE id = (:id)", id=t1p1)
+            db.execute("UPDATE players SET w = w + 1 WHERE id = (:id)", id=t2p1)
+            #flash message
+            flash("Result added!")
+            
+        #game type was team    
+        elif request.form.get("match_type") == 'team':
+            # get all player's id
+            t1p1 = request.form.get("t1p1")
+            t1p2 = request.form.get("t1p2")
+            t2p1 = request.form.get("t2p1")
+            t2p2 = request.form.get("t2p2")
+            #rearrange the id of the player alphabetically
+            t1order = db.execute("SELECT id FROM players WHERE id = (:t1p1id) OR id = (:t1p2id) ORDER BY name ASC", t1p1id=t1p1, t1p2id=t1p2)
+            t2order = db.execute("SELECT id FROM players WHERE id = (:t2p1id) OR id = (:t2p2id) ORDER BY name ASC", t2p1id=t2p1, t2p2id=t2p2)
+            #check if any team already created
+            t1teamcheck = db.execute("SELECT * FROM teams WHERE p1_id = (:t1p1id) AND p2_id = (:t1p2id)", t1p1id=t1order[0]['id'], t1p2id=t1order[1]['id'])
+            t2teamcheck = db.execute("SELECT * FROM teams WHERE p1_id = (:t2p1id) AND p2_id = (:t2p2id)", t2p1id=t2order[0]['id'], t2p2id=t2order[1]['id'])
+            #team 1 check
+            #if not found
+            if len(t1teamcheck) == 0:
+                db.execute("INSERT INTO teams (p1_id, p2_id, l) VALUES ((:p1_id),(:p2_id),(:l))", p1_id=t1order[0]['id'], p2_id=t1order[1]['id'], l=1)
+                db.execute("UPDATE players SET l = l + 1 WHERE id = (:id)", id=t1order[0]['id'])
+                db.execute("UPDATE players SET l = l + 1 WHERE id = (:id)", id=t1order[1]['id'])
+            #if found
+            else:
+                db.execute("UPDATE teams SET l = l + 1 WHERE p1_id = (:p1_id) AND p2_id = (:p2_id)", p1_id=t1order[0]['id'], p2_id=t1order[1]['id'])
+                db.execute("UPDATE players SET l = l + 1 WHERE id = (:id)", id=t1order[0]['id'])
+                db.execute("UPDATE players SET l = l + 1 WHERE id = (:id)", id=t1order[1]['id'])
+            #team 2 check
+            # if not found
+            if len(t2teamcheck) == 0:
+                db.execute("INSERT INTO teams (p1_id, p2_id, w) VALUES ((:p1_id),(:p2_id),(:w))", p1_id=t2order[0]['id'], p2_id=t2order[1]['id'], w=1)
+                db.execute("UPDATE players SET w = w + 1 WHERE id = (:id)", id=t2order[0]['id'])
+                db.execute("UPDATE players SET w = w + 1 WHERE id = (:id)", id=t2order[1]['id'])
+            #if found
+            else:
+                db.execute("UPDATE teams SET w = w + 1 WHERE p1_id = (:p1_id) AND p2_id = (:p2_id)", p1_id=t2order[0]['id'], p2_id=t2order[1]['id'])
+                db.execute("UPDATE players SET w = w + 1 WHERE id = (:id)", id=t2order[0]['id'])
+                db.execute("UPDATE players SET w = w + 1 WHERE id = (:id)", id=t2order[1]['id'])
+              
+            flash("Result added!")
+
     return redirect("/add-result")
 
 @app.route("/add-player", methods=["GET", "POST"])
@@ -70,7 +216,7 @@ def player_ranking():
 def team_ranking():
 
     #select the information required for the team ranking
-    teams = db.execute("SELECT p1.name as p1_name, p2.name as p2_name, teams.w, teams.d, teams.l FROM teams JOIN players p1 on teams.p1_id = p1.id JOIN players p2 on teams.p2_id = p2.id;")
+    teams = db.execute("SELECT p1.name as p1_name, p2.name as p2_name, teams.w, teams.d, teams.l FROM teams JOIN players p1 on teams.p1_id = p1.id JOIN players p2 on teams.p2_id = p2.id ORDER BY p1_name ASC")
 
     #render the page with the team information
     return render_template("team-ranking.html", teams=teams)    
